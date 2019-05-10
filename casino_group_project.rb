0d37897ee93599f6e91cddde506ea6929f58d569
Aaron Krishnan <buzzkri@gmail.com>
2019-05-10 14:27:38 -0700

    final first commit

---
+++ casino_group_project.rb
@@ -0,0 +1,5 @@
+require_relative './main_menu.rb'
+require_relative './black_jack.rb'
+require_relative './rock_paper_scissors.rb'
+require_relative './roulette.rb.'
+require_relative './slots.rb'
---
+++ rock_paper_scissors.rb
@@ -0,0 +1,14 @@
+
+
+class rock_paper_scissors
+  @rpc = ["Rock"]
+  
+  
+  end
+  
+  
+  # array with 3 options
+  # randomize results for opponent
+  # if function for each result
+  # then a prize and a loss for results
+  
