60f0b84e9113ca0c3844c7bf45a44d4066f2907a
agarcian031 <algarcian@outlook.com>
2019-05-10 17:43:01 -0600

    Uncommented relative paths

--- casino_group_project.rb
+++ casino_group_project.rb
@@ -1,9 +1,9 @@
 require_relative "./main_menu.rb"
-# require_relative "./black_jack.rb"
-# require_relative "./rock_paper_scissors.rb"
-# require_relative "./roulette.rb."
-# require_relative "./slots.rb"
-# require_relative "./high_low.rb"
+require_relative "./black_jack.rb"
+require_relative "./rock_paper_scissors.rb"
+require_relative "./roulette.rb."
+require_relative "./slots.rb"
+require_relative "./high_low.rb"
 
 class CasinoProject < Player
   def initialize
